c1f15338ae0f3b9c2b9b048ab289bf15e4229a7e
svmano <97649352+svmano@users.noreply.github.com>
2022-01-12 18:44:59 -0800

    Initial Version

---
+++ calc2/app/src/main/java/com/homefinch/calc/MainActivity.java
@@ -0,0 +1,157 @@
+package com.homefinch.calc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    String op = "";
+    String oldNumber = "";
+    boolean isNew = true;
+    EditText ed1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ed1 = findViewById(R.id.text);
+    }
+
+    public void numberEvent(View view) {
+        if(isNew)
+            ed1.setText("");
+        isNew = false;
+        String number = ed1.getText().toString();
+        switch(view.getId()) {
+            case R.id.bu1:
+                number += "1";
+                break;
+            case R.id.bu2:
+                number += "2";
+                break;
+            case R.id.bu3:
+                number += "3";
+                break;
+            case R.id.bu4:
+                number += "4";
+                break;
+            case R.id.bu5:
+                number += "5";
+                break;
+            case R.id.bu6:
+                number += "6";
+                break;
+            case R.id.bu7:
+                number += "7";
+                break;
+            case R.id.bu8:
+                number += "8";
+                break;
+            case R.id.bu9:
+                number += "9";
+                break;
+            case R.id.bu0:
+                number += "0";
+                break;
+            case R.id.budot:
+                if(number.isEmpty())
+                    number = "0";
+                number += ".";
+                break;
+            case R.id.buplusminus:
+                number = "-"+ number;
+                break;
+
+        }
+        ed1.setText(number);
+
+    }
+    public void operatorEvent(View view)
+    {
+        isNew = true;
+        oldNumber = ed1.getText().toString();
+        switch(view.getId())
+        {
+            case R.id.buplus:
+                op = "+";
+                break;
+            case R.id.buminus:
+                op = "-";
+                break;
+            case R.id.budivide:
+                op = "/";
+                break;
+            case R.id.bumultiply:
+                op = "X";
+                break;
+        }
+
+    }
+
+    public void EqualEvent(View view) {
+        isNew = true;
+        String newNumber = ed1.getText().toString();
+       // if(op == "")
+          //  oldNumber = "";
+        /*if(newNumber.isEmpty()) {
+            if (oldNumber.isEmpty()) {
+                ed1.setText("");
+            }
+            else
+            {
+                ed1.setText(oldNumber);
+            }
+        }
+        else if(oldNumber.isEmpty())
+        {
+            if(newNumber.isEmpty())
+                ed1.setText("");
+            else
+                ed1.setText(newNumber);
+        }*/
+//        else {
+            try {
+                Double result = Double.parseDouble(newNumber);
+
+                switch (op) {
+                    case "+":
+                        result = Double.parseDouble(oldNumber) + Double.parseDouble(newNumber);
+                        break;
+                    case "-":
+                        result = Double.parseDouble(oldNumber) - Double.parseDouble(newNumber);
+                        break;
+                    case "X":
+                        result = Double.parseDouble(oldNumber) * Double.parseDouble(newNumber);
+                        break;
+                    case "/":
+                        result = Double.parseDouble(oldNumber) / Double.parseDouble(newNumber);
+                        break;
+                    default:
+//                result = Double.parseDouble(newNumber);
+
+                }
+                ed1.setText(result.toString());
+
+            } catch (NumberFormatException e) {
+                ed1.setText("");
+            }
+            newNumber = "";
+            oldNumber = "";
+            op = "";
+//        }
+    }
+
+    public void ACEvent(View view) {
+        ed1.setText("0");
+        isNew = true;
+    }
+
+    public void percentEvent(View view) {
+        double percent = Double.parseDouble(ed1.getText().toString())/100;
+        ed1.setText(percent+"");
+        isNew = true;
+    }
+}
